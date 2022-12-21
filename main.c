12babeda5757def2db54f2c3765b7184a34835be
Posejdon22 <60738715+Posejdon22@users.noreply.github.com>
2022-12-21 19:23:05 +0100

    first version

---
+++ main.c
@@ -0,0 +1,35 @@
+/*
+ * pwmExample.c
+ *
+ * Created: 20.12.2022 18:31:15
+ * Author : mateu
+ */ 
+#define F_CPU 16000000UL             //define CPU frequency
+#include "avr/io.h"
+#include <util/delay.h>
+
+void PWM_set(){                                 // PWM setup function
+	DDRD |= (1<<PD7);                              //set PD7 as PWM output
+	TCCR2 |= (1<<WGM20)|(1<<WGM21);                //select Fast PWM mode by setting bits
+	TCCR2 |=(1<<COM21)|(1<<CS20)|(0<<CS21)|(0<<CS22);   //clear OC2 on compare match
+}
+
+int main ()
+{
+	unsigned char duty;
+	PWM_set();         //call PWM setup function
+	while (1)
+	{
+		for(duty=0; duty<255; duty++)   // 0 to max duty cycle
+		{
+			OCR2=duty;     //slowly increase the LED brightness
+			_delay_ms(10);
+		}
+		for(duty=0; duty>255; duty--)   // max to 0 duty cycle
+		{
+			OCR2=duty;     //slowly decrease the LED brightness
+			_delay_ms(10);
+		}
+	}
+}
+
